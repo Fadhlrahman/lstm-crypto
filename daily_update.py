abfb9ddc01929dac52bc209201fb2e6a58914066
Fadhlrahman <fitraarsil19@gmail.com>
2025-05-23 04:08:11 +0700

    pindahin folder train

--- daily_update.py
+++ daily_update.py
@@ -4,6 +4,6 @@ print("Update data dari CoinGecko...")
 subprocess.run(["python", "update_data.py"])
 
 print("Retraining model semua koin...")
-subprocess.run(["python", "train/train_lstm.py"])
+subprocess.run(["python", "train_lstm.py"])
 
 print("Selesai.")
